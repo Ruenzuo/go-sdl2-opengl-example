24e42182bc453e3b6a2a2fdc02d06205d39444f0
Renzo Crisostomo <renzo@crisostomo.me>
2018-07-19 23:01:44 +0200

    Add opengl window

--- main/main.go
+++ main/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+	// Game Boy resolution: 160x144
+	// 3X for HDPI screens
+	width := int32(160 * 3)
+	height := int32(144 * 3)
+	window, err := sdl.CreateWindow("go-sdl2-opengl-example", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, width, height, sdl.WINDOW_OPENGL)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+	}
 }
